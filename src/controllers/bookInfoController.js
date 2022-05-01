4525950ea2e3b1b18a3d1d53539047b9f0abc40d
haileyjpark <chocowjdgus@gmail.com>
2022-05-01 21:52:46 +0900

    2022-05-01

--- src/controllers/bookInfoController.js
+++ src/controllers/bookInfoController.js
@@ -2,19 +2,5 @@ const CreateBookInfo = require('../services/CreateBookInfo');
 
 module.exports = {
 
-  async createBookInfo(request) {
-    // Context
-    const { serviceLocator } = request.server.app;
-
-    // Input
-    const {
-      firstName, lastName, email, password,
-    } = request.payload;
-
-    // Treatment
-    const user = await CreateBookInfo(firstName, lastName, email, password, serviceLocator);
-
-    // Output
-    return serviceLocator.userSerializer.serialize(user);
-  },
+ 
 };
